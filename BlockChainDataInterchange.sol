c77439ce7017d304d9130a70de4021b6297b3c71
evanlyle2018 <42079507+evanlyle2018@users.noreply.github.com>
2018-12-12 00:37:34 -0500

    Update BlockChainDataInterchange.sol

--- BlockChainDataInterchange.sol
+++ BlockChainDataInterchange.sol
@@ -5,14 +5,24 @@ contract BlockchainDataInterchange {
     // declare the mapping used to generate the ANSI orders
     mapping(uint256 => string) public ANSImap;
     
+	// declare the constructor to populate the ANSI mapping
     constructor() public {
-       ANSImap[201] = "Vehicle Shipping Order";
-       ANSImap[317] = "Delivery/Pickup Order";
+       ANSImap[201] = "<?xml version="1.0" encoding="UTF-8" standalone="yes" ?> <EDI> <type>Vehicle Shipping Order</type> </EDI>";
+       ANSImap[317] = "<?xml version="1.0" encoding="UTF-8" standalone="yes" ?> <EDI> <type>Delivery/Pickup Order</type> </EDI>";
+	   ANSImap[850] = "<?xml version="1.0" encoding="UTF-8" standalone="yes" ?> <EDI> <type>Purchase Order</type> </EDI>";
     }
 
+	// This function takes the ANSI code and the address of the recipient
+	// This function will send the recipient the amount of money sent to 
+	// the contract and return the value of the ANSI XML
+	// more work is required to build the specific XML's for each ANSI code
     function sendBDI(uint256 ANSIcode, address receiver) public 
         payable returns (string){
+		
+		// transfer the money
         receiver.transfer(msg.value);
+		
+		//get the ANSI code
         string storage EDI = ANSImap[ANSIcode];
         return EDI;
     }
